0785047ba2f3d9962fc0004c075fb01d02e75eb2
congenial-compressor-7742 <tateejt@gmail.com>
2019-09-30 04:59:06 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -17,7 +17,7 @@ def hopper
      }
 
 end
-puts programmer_hash[:grace_hopper]
+p programmer_hash[:grace_hopper]
 
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
